57f9baded85c36c0a5d560d61aa5a40c36404443
cattyIT <yaqing.weiyq@gmail.com>
2022-01-06 17:00:36 +0800

    user的swagger2说明

--- case-server/src/main/java/com/xiaoju/framework/controller/UserController.java
+++ case-server/src/main/java/com/xiaoju/framework/controller/UserController.java
@@ -4,6 +4,9 @@ import com.xiaoju.framework.entity.request.auth.UserLoginReq;
 import com.xiaoju.framework.entity.request.auth.UserRegisterReq;
 import com.xiaoju.framework.entity.response.controller.Response;
 import com.xiaoju.framework.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by didi on 2021/4/22.
  */
+@Api(tags = "用户管理模块")
 @CrossOrigin
 @RestController
 @RequestMapping("/api/user")
@@ -26,6 +30,7 @@ public class UserController {
      * @param response
      * @return
      */
+    @ApiOperation("用户注册接口")
     @PostMapping("/register")
     public Response<?> register(@RequestBody UserRegisterReq req, HttpServletRequest request, HttpServletResponse response) {
         return Response.success(userService.register(req,request,response));
@@ -38,11 +43,13 @@ public class UserController {
      * @param response
      * @return
      */
+    @ApiOperation("用户登录接口")
     @PostMapping("/login")
     public Response<?> login(@RequestBody UserLoginReq req, HttpServletRequest request, HttpServletResponse response) {
         return Response.success(userService.login(req,request,response));
     }
 
+    @ApiOperation("用户退出接口")
     @PostMapping("/quit")
     public Response<?> logout(HttpServletRequest request,HttpServletResponse response) {
         return Response.success(userService.logout(request, response));
--- case-server/src/main/java/com/xiaoju/framework/entity/request/auth/UserLoginReq.java
+++ case-server/src/main/java/com/xiaoju/framework/entity/request/auth/UserLoginReq.java
@@ -1,13 +1,17 @@
 package com.xiaoju.framework.entity.request.auth;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * Created by didi on 2021/4/22.
  */
+@ApiModel(value = "用户登录接口实体类", description = "请求类")
 @Data
 public class UserLoginReq {
+    @ApiModelProperty(value = "用户名称", example = "username", required = true)
     private String username;
-
+    @ApiModelProperty(value = "用户密码", example = "password", required = true)
     private String password;
 }
